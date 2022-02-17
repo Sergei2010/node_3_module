c0ecbbcb24a6aeda46dd53bce02cbf0b470e0240
sergei2010 <sergejmatvejcuk323@gmail.com>
2022-02-17 13:26:18 +0200

    add edit method

--- index.js
+++ index.js
@@ -1,51 +1,77 @@
-const yargs = require('yargs')
+const express = require('express')
+const chalk = require('chalk')
+// const fs = require('fs/promises')
+const path = require('path')
+const { addNote, getNotes, removeNote, updateNote } = require('./notes.controller')
 
-// гибкая настройка получения версий
-const pkg = require('./package.json')
-const { addNote, printNotes, removeNote } = require('./notes.controller')
+const port = 3000
 
-yargs.version(pkg.version)
+const app = express()
 
-// регистрируем команду "add"
-yargs.command({
-	command: 'add',
-	describe: 'Add new note to list',
-	// с какими полями инициировать заментку
-	builder: {
-		title: {
-			type: 'string',
-			describe: 'Note title',
-			demandOption: true
-		}
-	},
-	handler({ title }) {
-		addNote(title)
-	}
+// переопределяем папку с файлами
+app.set('views', 'pages')
+
+// определяем шаблонизатор 'ejs'
+app.set('view engine', 'ejs')
+
+// необходимо обозначить, что папка "public" - статическая
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.use(express.json())
+
+// с какими данными работаем
+app.use(
+	express.urlencoded({
+		extended: true
+	})
+)
+
+// неободимо обработать GET запрос
+app.get('/', async (req, res) => {
+	// перелаваемые параметры
+	res.render('index', {
+		title: 'Express App',
+		notes: await getNotes(),
+		created: false
+	})
 })
 
-// регистрируем команду "list"
-yargs.command({
-	command: 'list',
-	describe: 'Print all notes',
-	async handler() {
-		printNotes()
-	}
+// неободимо обработать POST запрос
+app.post('/', async (req, res) => {
+	await addNote(req.body.title)
+
+	// перелаваемые параметры
+	res.render('index', {
+		title: 'Express App',
+		notes: await getNotes(),
+		created: true // флаг создания note для alert
+	})
 })
 
-yargs.command({
-	command: 'remove',
-	describe: 'Remove note by id',
-	builder: {
-		id: {
-			type: 'string',
-			describe: 'Note uniq id',
-			demandOption: true
-		}
-	},
-	async handler({ id }) {
-		removeNote(id)
-	}
+// для удаления определённой note
+app.delete('/:id', async (req, res) => {
+	await removeNote(req.params.id)
+
+	// перелаваемые параметры
+	res.render('index', {
+		title: 'Express App',
+		notes: await getNotes(),
+		created: false // флаг создания note для alert
+	})
 })
 
-// инициализируем зарегистрированные команды
-yargs.parse()
+// редактирование данных
+app.put('/:id', async (req, res) => {
+	await updateNote({ id: req.params.id, title: req.body.title })
+
+	// перелаваемые параметры
+	res.render('index', {
+		title: 'Express App',
+		notes: await getNotes(),
+		created: false // флаг создания note для alert
+	})
+})
+
+app.listen(port, () => {
+	console.log(chalk.green(`Server has been started on port ${port}...`))
+})
---
+++ public/app.js
@@ -0,0 +1,39 @@
+document.addEventListener('click', (event) => {
+	if (event.target.dataset.type === 'remove') {
+		const id = event.target.dataset.id
+
+		// необходимо отправить запрос
+		remove(id).then(() => {
+			event.target.closest('li').remove()
+		})
+	}
+	// для редактирования
+	if (event.target.dataset.type === 'edit') {
+		const id = event.target.dataset.id
+		const title = event.target.dataset.title
+		const newTitle = prompt('Введите новое название', title)
+		console.log('newTitle: ', newTitle)
+		if (newTitle !== null) {
+			update({ id, title: newTitle }).then(() => {
+				event.target.closest('li').querySelector('span').innerText = newTitle
+			})
+		}
+	}
+})
+
+async function update(newNote) {
+	await fetch(`/${newNote.id}`, {
+		method: 'PUT',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(newNote)
+	})
+}
+
+async function remove(id) {
+	await fetch(`/${id}`, {
+		method: 'DELETE'
+	})
+}
